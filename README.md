b4ccd59c0d198f9b4b4cab913aa114422c549080
impossible98 <christoleavenow@outlook.com>
2024-08-24 05:50:09 +0800

    feat: complete youtube

---
+++ README.md
@@ -0,0 +1,9 @@
+# Scripts
+
+## Video Check
+
+```bash
+cp .env.example .env
+node ./dist/video.js --file "<file>" --url "<url>"
+# https://console.cloud.google.com/apis/credentials
+```
--- video.ts
+++ video.ts
@@ -4,7 +4,9 @@
 import fs from 'fs';
 import path from 'path';
 // import third-party modules
+import axios from 'axios';
 const argv = require('minimist')(process.argv.slice(2));
+require('dotenv').config();
 
 const bilibiliURL: string = 'https://www.bilibili.com/video/';
 const youtubeURL: string = 'https://www.youtube.com/watch';
@@ -24,22 +26,95 @@ function parseYoutubeID(url: string) {
   return id;
 }
 
-async function fetchData(id: string): Promise<string | undefined> {
+async function fetchBilibiliData(id: string) {
   try {
-    const response = await fetch(
-      'https://api.bilibili.com/x/web-interface/view?bvid=' + id.slice(2),
+    const response = await axios.get(
+      `https://api.bilibili.com/x/web-interface/view?bvid=${id.slice(2)}`,
     );
-    if (!response.ok) {
+    if (response.status !== 200) {
       throw new Error(`HTTP error! status: ${response.status}`);
     }
-    const data = await response.text(); // 假设返回的是 JSON 数据
+    const data = await response.data;
+    // return
     return data;
   } catch (error) {
     console.error('Failed to fetch:', error);
   }
 }
-// 移动文件并重命名
-function moveFile(
+
+async function fetchYoutubeData(id: string) {
+  const key = process.env.YOUTUBE_KEY;
+  const PROXY_HOST = process.env.PROXY_HOST;
+  const PROXY_PORT = process.env.PROXY_PORT;
+  if (!key) {
+    console.error('Please set the YOUTUBE_KEY environment variable.');
+    return;
+  }
+  if (!PROXY_HOST || !PROXY_PORT) {
+    console.error(
+      'Please set the PROXY_HOST and PROXY_PORT environment variables.',
+    );
+    return;
+  }
+  try {
+    const response = await axios.get(
+      `https://youtube.googleapis.com/youtube/v3/videos?part=snippet&id=${id}&key=${key}`,
+      {
+        proxy: {
+          protocol: 'http',
+          host: PROXY_HOST,
+          port: parseInt(PROXY_PORT, 10),
+        },
+      },
+    );
+    if (response.status !== 200) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    const data = await response.data;
+    // return
+    return data;
+  } catch (error) {
+    console.error('Failed to fetch:', error);
+  }
+}
+
+async function fetchYoutubeChannelData(channelId: string) {
+  const key = process.env.YOUTUBE_KEY;
+  const PROXY_HOST = process.env.PROXY_HOST;
+  const PROXY_PORT = process.env.PROXY_PORT;
+  if (!key) {
+    console.error('Please set the YOUTUBE_KEY environment variable.');
+    return;
+  }
+  if (!PROXY_HOST || !PROXY_PORT) {
+    console.error(
+      'Please set the PROXY_HOST and PROXY_PORT environment variables.',
+    );
+    return;
+  }
+  try {
+    const response = await axios.get(
+      `https://youtube.googleapis.com/youtube/v3/channels?part=snippet&id=${channelId}&key=${key}`,
+      {
+        proxy: {
+          protocol: 'http',
+          host: PROXY_HOST,
+          port: parseInt(PROXY_PORT, 10),
+        },
+      },
+    );
+    if (response.status !== 200) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    const data = await response.data;
+    // return
+    return data;
+  } catch (error) {
+    console.error('Failed to fetch:', error);
+  }
+}
+
+function moveBilibiliFile(
   file: string,
   timestamp: number,
   name: string,
@@ -51,7 +126,7 @@ function moveFile(
     if (err) {
       throw err;
     }
-    console.log('文件创建时间已修改为 ' + formatDate(timestamp));
+    console.log('文件创建时间已修改为 ' + formatBilibiliDate(timestamp));
   });
   const oldPath = path.resolve(file);
   const newPath = path.join(
@@ -88,7 +163,57 @@ function moveFile(
   });
 }
 
-function formatDate(timestamp: number): string {
+function moveYoutubeFile(
+  file: string,
+  publishTime: string,
+  name: string,
+  uid: string,
+  uname: string,
+) {
+  let timestamp: number = new Date(publishTime).getTime() / 1000;
+  // 修改文件的创建时间
+  fs.utimes(file, timestamp, timestamp, function (err: any) {
+    if (err) {
+      throw err;
+    }
+    console.log('文件创建时间已修改为 ' + formatYoutubeDate(publishTime));
+  });
+  const oldPath = path.resolve(file);
+  const newPath = path.join(
+    path.dirname(file),
+    'completed',
+    `${uname}(${uid}) - youtube.com`,
+    `${name}${path.extname(file)}`,
+  );
+  // 创建文件夹如果不存在
+  if (
+    !fs.existsSync(
+      path.join(
+        path.dirname(file),
+        'completed',
+        `${uname}(${uid}) - youtube.com`,
+      ),
+    )
+  ) {
+    fs.mkdirSync(
+      path.join(
+        path.dirname(file),
+        'completed',
+        `${uname}(${uid}) - youtube.com`,
+      ),
+      { recursive: true },
+    );
+  }
+  // 移动文件并重命名
+  fs.rename(oldPath, newPath, function (err: any) {
+    if (err) {
+      throw err;
+    }
+    console.log('File renamed!');
+  });
+}
+
+function formatBilibiliDate(timestamp: number): string {
   const date = new Date(timestamp * 1000);
   const year = date.getFullYear().toString();
   const month = (date.getMonth() + 1).toString().padStart(2, '0');
@@ -100,27 +225,60 @@ function formatDate(timestamp: number): string {
   return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
 }
 
+function formatYoutubeDate(date: string): string {
+  const dateObj = new Date(date);
+  const year = dateObj.getFullYear().toString();
+  const month = (dateObj.getMonth() + 1).toString().padStart(2, '0');
+  const day = dateObj.getDate().toString().padStart(2, '0');
+  const hours = dateObj.getHours().toString().padStart(2, '0');
+  const minutes = dateObj.getMinutes().toString().padStart(2, '0');
+  const seconds = dateObj.getSeconds().toString().padStart(2, '0');
+
+  return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+}
 function main() {
   if (ARGVURL.startsWith(bilibiliURL)) {
     let id = parseBilibiliID(ARGVURL);
     if (id) {
-      fetchData(id).then((data) => {
+      fetchBilibiliData(id).then((data) => {
         if (data) {
-          let id = JSON.parse(data).data.bvid.slice(2);
-          let publishTime = JSON.parse(data).data.pubdate;
-          let title = JSON.parse(data).data.title;
-          let uid = JSON.parse(data).data.owner.mid;
-          let name = JSON.parse(data).data.owner.name;
+          let id: string = data.data.bvid.slice(2);
+          let publishTime: number = data.data.pubdate;
+          let title: string = data.data.title;
+          let uid: string = data.data.owner.mid;
+          let name: string = data.data.owner.name;
           console.log(`ID:          ` + id);
           console.log(`Title:       ` + title);
-          console.log(`PublishTime: ` + formatDate(publishTime));
+          console.log(`PublishTime: ` + formatBilibiliDate(publishTime));
           let safeTitle = title.replace(/[/|\\:?<>*"]/g, '');
-          moveFile(file, publishTime, safeTitle, uid, name);
+          moveBilibiliFile(file, publishTime, safeTitle, uid, name);
         }
       });
     }
   } else if (ARGVURL.startsWith(youtubeURL)) {
-    console.log(parseYoutubeID(ARGVURL));
+    let id = parseYoutubeID(ARGVURL);
+    if (id) {
+      fetchYoutubeData(id).then((data) => {
+        if (data) {
+          let channelId: string = data.items[0].snippet.channelId;
+          fetchYoutubeChannelData(channelId).then((data2) => {
+            if (data2) {
+              let id: string = data.items[0].id;
+              let publishTime: string = data.items[0].snippet.publishedAt;
+              let title: string = data.items[0].snippet.title;
+              let uid = data2.items[0].snippet.customUrl.slice(1)
+              let name: string = data.items[0].snippet.channelTitle;
+              console.log(`ID:          ` + id);
+              console.log(`Title:       ` + title);
+              console.log(`PublishTime: ` + formatYoutubeDate(publishTime));
+              let safeTitle = title.replace(/[/|\\:?<>*"]/g, '');
+              moveYoutubeFile(file, publishTime, safeTitle, uid, name);
+            }
+          });
+
+        }
+      });
+    }
   } else {
     console.log('other');
   }
